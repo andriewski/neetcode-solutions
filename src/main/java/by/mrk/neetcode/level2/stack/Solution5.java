11e73eec50233005146823d3323340a750754297
Mark Andryieuski <plambiroo@gmail.com>
2026-09-06 20:25:42 +0200

    Alternative cleanup

--- src/main/java/by/mrk/neetcode/level2/stack/Solution5.java
+++ src/main/java/by/mrk/neetcode/level2/stack/Solution5.java
@@ -1,38 +1,31 @@
 package by.mrk.neetcode.level2.stack;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Deque;
 
 /**
  * <a href="https://neetcode.io/problems/car-fleet/question">Car Fleet</a>
  */
 public class Solution5 {
 
-    public static void main(String[] args) {
-        Solution5 s = new Solution5();
-        System.out.println(s.carFleet(10, new int[] {1, 4}, new int[] {3, 2}));
-        System.out.println(s.carFleet(10, new int[] {4, 1, 0, 7}, new int[] {2, 2, 1, 1}));
-        System.out.println(s.carFleet(12, new int[] {10, 8, 0, 5, 3}, new int[] {2, 4, 1, 1, 3}));
-    }
-
     public int carFleet(int target, int[] position, int[] speed) {
         Car[] cars = toCars(target, position, speed);
 
         Arrays.sort(cars, Comparator.comparing(Car::position).reversed());
 
-        int counter = 0;
-        double time =  Double.NEGATIVE_INFINITY;
+        Deque<Double> carFleetTimes = new ArrayDeque<>();
 
         for (Car car : cars) {
             double newTime = car.time2Ride;
 
-            if (newTime > time) {
-                counter++;
-                time = newTime;
+            if (carFleetTimes.isEmpty() || newTime > carFleetTimes.peek()) {
+                carFleetTimes.push(newTime);
             }
         }
 
-        return counter;
+        return carFleetTimes.size();
     }
 
     private Car[] toCars(int target, int[] position, int[] speed) {
